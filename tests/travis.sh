70ce9cf5e9fbd4d68a871761e99cf86d8adfed0e
Arash Pajooh <apajoohande@gmail.com>
2012-05-23 23:37:01 +0430

    updated tests for postgresql

--- tests/travis.sh
+++ tests/travis.sh
@@ -5,8 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git submodule update --init --recursive
 
 #mysql -e 'create database IF NOT EXISTS phpcr_tests;' -u root
-psql -c "DROP DATABASE IF EXISTS phpcr_test;" -U postgres
-psql -c "create database phpcr_test;" -U postgres
+psql -c "DROP DATABASE IF EXISTS phpcr_tests;" -U postgres
+psql -c "create database phpcr_tests;" -U postgres
 
 pyrus install phpunit/DBUnit
 
