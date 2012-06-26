0cf8772db84d5062d25931ca31bad16063980c63
Crypto Compress <cryptocompress@googlemail.com>
2012-06-26 13:39:29 +0300

    Update master

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php') {
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
         'php composer.phar install'.PHP_EOL);
