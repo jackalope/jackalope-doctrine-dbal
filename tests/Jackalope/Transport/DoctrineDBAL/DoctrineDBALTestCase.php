6569f66a9a12a9b0b978ef1d0af732fee64f76cc
Crypto Compress <cryptocompress@googlemail.com>
2012-07-17 22:54:56 +0300

    create new connection only if there is no global connection available

--- tests/Jackalope/Transport/DoctrineDBAL/DoctrineDBALTestCase.php
+++ tests/Jackalope/Transport/DoctrineDBAL/DoctrineDBALTestCase.php
@@ -12,14 +12,19 @@ abstract class DoctrineDBALTestCase extends TestCase
     protected function getConnection()
     {
         if ($this->conn === null) {
-            $this->conn = DriverManager::getConnection(array(
-                'driver'    => @$GLOBALS['phpcr.doctrine.dbal.driver'],
-                'path'      => @$GLOBALS['phpcr.doctrine.dbal.path'],
-                'host'      => @$GLOBALS['phpcr.doctrine.dbal.host'],
-                'user'      => @$GLOBALS['phpcr.doctrine.dbal.username'],
-                'password'  => @$GLOBALS['phpcr.doctrine.dbal.password'],
-                'dbname'    => @$GLOBALS['phpcr.doctrine.dbal.dbname']
-            ));
+            global $dbConn;
+            $this->conn = $dbConn;
+
+            if ($this->conn === null) {
+                $this->conn = DriverManager::getConnection(array(
+                    'driver'    => @$GLOBALS['phpcr.doctrine.dbal.driver'],
+                    'path'      => @$GLOBALS['phpcr.doctrine.dbal.path'],
+                    'host'      => @$GLOBALS['phpcr.doctrine.dbal.host'],
+                    'user'      => @$GLOBALS['phpcr.doctrine.dbal.username'],
+                    'password'  => @$GLOBALS['phpcr.doctrine.dbal.password'],
+                    'dbname'    => @$GLOBALS['phpcr.doctrine.dbal.dbname']
+                ));
+            }
         }
         return $this->conn;
     }
