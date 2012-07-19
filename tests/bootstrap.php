7de0a61c7b7fc8448e793cc5d4d73c95ead326df
cryptocompress <cryptocompress@googlemail.com>
2012-07-19 18:58:09 +0200

    clean up the way tests can be run

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,18 @@
 <?php
 
-if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
-    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
-        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
-        'php composer.phar install'.PHP_EOL);
-}
-
-$loader->add('Jackalope', __DIR__.'/../vendor/jackalope/jackalope/tests');
-
 /** make sure we get ALL infos from php */
 error_reporting(E_ALL | E_STRICT);
+
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -s http://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL .
+        'php composer.phar install --dev' . PHP_EOL);
+}
+
+require_once 'generate_fixtures.php';
+
+$loader->add('Jackalope', __DIR__.'/../vendor/jackalope/jackalope/tests');
 
 ### Load classes needed for jackalope unit tests ###
 require 'Jackalope/Transport/DoctrineDBAL/DoctrineDBALTestCase.php';
