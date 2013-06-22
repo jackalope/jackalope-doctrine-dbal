b9858f62923d50953fe3a3c01cc637cda9a211ce
Willem-Jan Zijderveld <willemjan@beeldspraak.com>
2013-06-22 02:05:53 +0200

    Removed the test from unsupportedTests in ImplementationLoader
    Signed-off-by: Willem-Jan Zijderveld <willemjan@beeldspraak.com>

--- tests/ImplementationLoader.php
+++ tests/ImplementationLoader.php
@@ -69,8 +69,6 @@ class ImplementationLoader extends \PHPCR\Test\AbstractLoader
                     'Query\\QueryManagerTest::testGetQuery',
                     'Query\\QueryManagerTest::testGetQueryInvalid',
                     'Query\\QueryObjectSql2Test::testGetStoredQueryPath',
-                    // TODO https://github.com/jackalope/jackalope-doctrine-dbal/issues/88
-                    // 'Query\QuerySql2OperationsTest::testQueryMultiValuedProperty',
                     // TODO https://github.com/jackalope/jackalope-doctrine-dbal/issues/89 (next two tests)
                     'Query\QueryResultsTest::testCompareNumberFields',
                     'Query\QueryResultsTest::testBooleanField',
