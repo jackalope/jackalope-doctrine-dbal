a82d1d91b97183ef9e55ef58b84e16863c286347
Daniel Rotter <daniel.rotter@gmail.com>
2015-04-02 19:32:42 +0200

    implemented VersioningInterface

--- src/Jackalope/Transport/DoctrineDBAL/Client.php
+++ src/Jackalope/Transport/DoctrineDBAL/Client.php
@@ -2,6 +2,7 @@
 
 namespace Jackalope\Transport\DoctrineDBAL;
 
+use Jackalope\Transport\VersioningInterface;
 use PHPCR\LoginException;
 use PHPCR\NodeType\NodeDefinitionInterface;
 use PHPCR\NodeType\NodeTypeExistsException;
@@ -24,6 +25,7 @@ use PHPCR\PathNotFoundException;
 use PHPCR\Query\InvalidQueryException;
 use PHPCR\NodeType\ConstraintViolationException;
 
+use PHPCR\UnsupportedRepositoryOperationException;
 use PHPCR\Util\QOM\Sql2ToQomQueryConverter;
 use PHPCR\Util\ValueConverter;
 use PHPCR\Util\UUIDHelper;
@@ -52,6 +54,7 @@ use Jackalope\NodeType\NodeTypeDefinition;
 use Jackalope\FactoryInterface;
 use Jackalope\NotImplementedException;
 use Jackalope\NodeType\NodeProcessor;
+use PHPCR\Version\VersionException;
 
 /**
  * Class to handle the communication between Jackalope and RDBMS via Doctrine DBAL.
@@ -62,7 +65,7 @@ use Jackalope\NodeType\NodeProcessor;
  * @author Benjamin Eberlei <kontakt@beberlei.de>
  * @author Lukas Kahwe Smith <smith@pooteeweet.org>
  */
-class Client extends BaseTransport implements QueryTransport, WritingInterface, WorkspaceManagementInterface, NodeTypeManagementInterface, TransactionInterface
+class Client extends BaseTransport implements QueryTransport, WritingInterface, WorkspaceManagementInterface, NodeTypeManagementInterface, TransactionInterface, VersioningInterface
 {
     /**
      * SQlite can only handle a maximum of 999 parameters inside an IN statement
@@ -2466,5 +2469,63 @@ class Client extends BaseTransport implements QueryTransport, WritingInterface, 
         }
 
         $this->connectionInitialized = true;
+    }
+
+    /**
+     * Check-in item at path.
+     *
+     * @see VersionManager::checkin
+     *
+     * @param string $path absolute path to the node
+     *
+     * @return string path to the new version
+     *
+     * @throws UnsupportedRepositoryOperationException
+     * @throws RepositoryException
+     */
+    public function checkinItem($path)
+    {
+        // TODO: Implement checkinItem() method.
+    }
+
+    /**
+     * Check-out item at path.
+     *
+     * @see VersionManager::checkout
+     *
+     * @param string $path absolute path to the node
+     *
+     * @throws UnsupportedRepositoryOperationException
+     * @throws RepositoryException
+     */
+    public function checkoutItem($path)
+    {
+        // TODO: Implement checkoutItem() method.
+    }
+
+    /**
+     * Restore the item at versionPath to the location path
+     *
+     * TODO: This is incomplete. Needs batch processing to avoid chicken-and-egg problems
+     *
+     * @see VersionManager::restoreItem
+     */
+    public function restoreItem($removeExisting, $versionPath, $path)
+    {
+        // TODO: Implement restoreItem() method.
+    }
+
+    /**
+     * Remove a version given the path to the version node and the name of the version.
+     *
+     * @param string $versionPath absolute path to the version node
+     * @param string $versionName The name of the version
+     *
+     * @throws ReferentialIntegrityException
+     * @throws VersionException
+     */
+    public function removeVersion($versionPath, $versionName)
+    {
+        // TODO: Implement removeVersion() method.
     }
 }
