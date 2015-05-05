f1287562ccb8aefe8d505ee439c058a517fad4a9
Daniel Rotter <daniel.rotter@gmail.com>
2015-05-05 10:15:32 +0200

    added call to checkout method

--- src/Jackalope/Transport/DoctrineDBAL/Client.php
+++ src/Jackalope/Transport/DoctrineDBAL/Client.php
@@ -2521,7 +2521,7 @@ class Client extends BaseTransport implements QueryTransport, WritingInterface, 
      */
     public function checkoutItem($path)
     {
-        // TODO: Implement checkoutItem() method.
+        return $this->getVersionHandler()->checkoutItem($path);
     }
 
     /**
