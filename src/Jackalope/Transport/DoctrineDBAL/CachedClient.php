a156b2e44fb20ccf94ef9d8931249ae0af0d05cc
Lukas Kahwe Smith <smith@pooteeweet.org>
2015-06-18 13:49:22 +0200

    fixed handling of getNodeTypes() when not filtering for specific node types in the cached client

--- src/Jackalope/Transport/DoctrineDBAL/CachedClient.php
+++ src/Jackalope/Transport/DoctrineDBAL/CachedClient.php
@@ -84,13 +84,13 @@ class CachedClient extends Client
     public function getNodeTypes($nodeTypes = array())
     {
         $cacheKey = 'nodetypes: '.serialize($nodeTypes);
-        $nodeTypes = $this->caches['meta']->fetch($cacheKey);
-        if (!$nodeTypes) {
-            $nodeTypes = parent::getNodeTypes($nodeTypes);
-            $this->caches['meta']->save($cacheKey, $nodeTypes);
+        $result = $this->caches['meta']->fetch($cacheKey);
+        if (!$result) {
+            $result = parent::getNodeTypes($nodeTypes);
+            $this->caches['meta']->save($cacheKey, $result);
         }
 
-        return $nodeTypes;
+        return $result;
     }
 
     /**
