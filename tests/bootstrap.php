8224bc79b2a9142e9226b1b6f75096996e9618bc
David Buchmann <david.buchmann@liip.ch>
2015-08-11 08:38:09 +0200

    cleanup test loading and update jackalope and tests version

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-/** make sure we get ALL infos from php */
+/** Make sure we get ALL infos from php */
 error_reporting(E_ALL | E_STRICT);
 
 if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
@@ -10,21 +10,24 @@ if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
         'php composer.phar install --dev' . PHP_EOL);
 }
 
-$loader->add('Jackalope\\Test', __DIR__);
+/** Make Jackalope base tests autoloadable */
 $loader->add('Jackalope', __DIR__ . '/../vendor/jackalope/jackalope/tests');
 
+/** Load the implementation loader class */
 require_once __DIR__ . '/ImplementationLoader.php';
 require_once __DIR__ . '/generate_fixtures.php';
 
-// generate fixtures
+/** generate fixtures */
 generate_fixtures(
     __DIR__ . '/../vendor/phpcr/phpcr-api-tests/fixtures',
     __DIR__ . '/fixtures/doctrine'
 );
 
-// set up the backend connection
-// For further details, please see Doctrine configuration page.
-// http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connection-details
+/**
+ * set up the backend connection
+ * For further details, please see Doctrine configuration page.
+ * http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connection-details
+ */
 $dbConn = \Doctrine\DBAL\DriverManager::getConnection(array(
     'driver'    => @$GLOBALS['phpcr.doctrine.dbal.driver'],
     'path'      => @$GLOBALS['phpcr.doctrine.dbal.path'],
@@ -35,14 +38,14 @@ $dbConn = \Doctrine\DBAL\DriverManager::getConnection(array(
     'dbname'    => @$GLOBALS['phpcr.doctrine.dbal.dbname']
 ));
 
-// recreate database schema
+/** Recreate database schema */
 if (!getenv('JACKALOPE_NO_TEST_DB_INIT')) {
     $options = array('disable_fks' => $dbConn->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\SqlitePlatform);
     $repositorySchema = new \Jackalope\Transport\DoctrineDBAL\RepositorySchema($options, $dbConn);
     $repositorySchema->reset();
 }
 
-/*
+/**
  * constants for the repository descriptor test for JCR 1.0/JSR-170 and JSR-283 specs
  */
 
