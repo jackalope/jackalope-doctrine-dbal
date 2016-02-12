abfca3784f7ed7fe76eafec538cadd3b4c0a9db3
Richard Snijders <richard@fizz.nl>
2016-02-12 15:52:56 +0100

    StyleCI fix

--- src/Jackalope/Transport/DoctrineDBAL/Query/QOMWalker.php
+++ src/Jackalope/Transport/DoctrineDBAL/Query/QOMWalker.php
@@ -299,14 +299,13 @@ class QOMWalker
      *
      * @throws \BadMethodCallException if the provided JoinCondition has no valid way of getting the right selector
      */
-    private function getRightJoinSelector(QOM\JoinConditionInterface $right) {
+    private function getRightJoinSelector(QOM\JoinConditionInterface $right)
+    {
         if($right instanceof QOM\ChildNodeJoinConditionInterface) {
             return $right->getParentSelectorName();
-        }
-        elseif($right instanceof QOM\DescendantNodeJoinConditionInterface) {
+        } elseif($right instanceof QOM\DescendantNodeJoinConditionInterface) {
             return $right->getAncestorSelectorName();
-        }
-        elseif(method_exists($right, 'getSelector2Name')) {
+        } elseif(method_exists($right, 'getSelector2Name')) {
             return $right->getSelector2Name();
         }
         throw new \BadMethodCallException('Supplied join type should implement getSelector2Name() or be an instance of ChildNodeJoinConditionInterface or DescendantNodeJoinConditionInterface');
